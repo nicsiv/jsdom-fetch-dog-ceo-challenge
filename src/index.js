804a46dae0e8a916018a7a7a84a3ab308aa5c03f
nicsiv <nicoleusivek@gmail.com>
2021-02-03 13:49:34 -0600

    final commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,53 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4" 
+function fetchImageInfo(){
+    fetch(imgUrl)
+    .then( res => res.json() )
+    .then( imageInfo => {
+        addImagesToDOM(imageInfo)
+    })
+}
+function addImagesToDOM(imageInfo){
+    let dogContainer = document.querySelector("#dog-image-container")
+    imageInfo.message.forEach(function(dog){
+        let newImage = document.createElement('img') 
+        newImage.setAttribute("src", dog)
+        dogContainer.append(newImage)
+    })
+}
+const breedUrl = 'https://dog.ceo/api/breeds/list/all' 
+function fetchBreedInfo(){
+    fetch(breedUrl)
+    .then( res => res.json() )
+    .then( breedInfo => {
+        addBreedsToDOM(breedInfo)
+    })
+}
+function addBreedsToDOM(breedInfo){
+    let breedContainer = document.querySelector("#dog-breeds")
+    Object.keys(breedInfo["message"]).forEach(function(dogBreed){
+        let newBreed = document.createElement('li') 
+        newBreed.innerText = dogBreed
+        breedContainer.append(newBreed)
+    })
+}
+function addingEventListener(){
+    // let dogList = document.querySelector
+}
+fetchImageInfo()
+fetchBreedInfo()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
